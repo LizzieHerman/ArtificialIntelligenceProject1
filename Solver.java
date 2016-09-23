6d78ff0901580d9d14f2b09bc99fbc09788e4ce8
LizzieHerman <lizziemn96@gmail.com>
2016-09-22 19:53:36 -0600

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,15 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package graphcoloring;
+
+import java.util.*;
 
 /**
  *
- * @author Lizzi
+ * @author Lizzie Herman
  */
 public abstract class Solver {
-    public abstract Graph solve(Graph g, int k);
-    public abstract boolean evaluate(Graph g, int k);
+    Comparator connections = new Comparator<Vertex>(){ // used to sort vertex list by highest num connections
+        public int compare(Vertex v1, Vertex v2){
+            if(v1.getConnections().isEmpty() && ! v2.getConnections().isEmpty()) return 1;
+            if(v2.getConnections().isEmpty() && ! v1.getConnections().isEmpty()) return -1;
+            if(v1.getConnections().isEmpty() && v2.getConnections().isEmpty()) return 0;
+            return v2.getConnections().size() - v1.getConnections().size();
+        }
+    };
+    Comparator possColors = new Comparator<Vertex>(){ // used to sort vertex list by lowest num of poss colors
+        public int compare(Vertex v1, Vertex v2){
+            if(v1.getPossColors().isEmpty() && ! v2.getPossColors().isEmpty()) return -1;
+            if(v2.getPossColors().isEmpty() && ! v1.getPossColors().isEmpty()) return 1;
+            if(v1.getPossColors().isEmpty() && v2.getPossColors().isEmpty()) return 0;
+            return v1.getPossColors().size() - v2.getPossColors().size();
+        }
+    };
+    
+    public abstract Graph solve(Graph g, int k); // solve method changes for each algorithm
+    
+    public boolean evaluate(Graph g){
+        for(Edge e: g.getEdges()){
+            int a = e.getFirstVert().getColor();
+            int b = e.getSecVert().getColor();
+            if(a == b) return false; // sees if any edge has the same color
+        }
+        return true;
+    }
 }
