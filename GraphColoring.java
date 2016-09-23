db482762d52f50a32d466b0375d1a0de45cc99ac
LizzieHerman <lizziemn96@gmail.com>
2016-09-23 13:29:50 -0600

    Update GraphColoring.java

--- GraphColoring.java
+++ GraphColoring.java
@@ -2,32 +2,63 @@ package graphcoloring;
 
 /**
  *
- * @author Lizzie Herman
+ * @author Lizzie Herman and Connor O'Leary
  */
+ 
+import java.util.*;
+
 public class GraphColoring {
     public static void main(String[] args) {
-        for(int i = 10; i <= 100; i += 10){
-            Graph g = new Graph(i);
-            Solver solve = new BackTrackingS(); // change this to change what algorithm using
-            g = solve.solve(g, 3);
-            System.out.println("Number of Vertices: " + i + " Number of Colors: 3");
-            if(g.getVertices() == null){
-                System.out.print("Graph could not be colored in 3 colors.");
-            }else{
-                if(solve.evaluate(g)) System.out.print("The Graph was colored correctly.");
-                else System.out.print("The algorithm did not work properly.");
+        boolean run = true;
+        while(run){
+            Solver solve  = new BackTrackingS();
+            System.out.println("Select which type of algorithm you want to use\nMin Conflicts: 0; Simple Backtracking: 1; Forward Checking: 2; MAC: 3; Genetic: 4; Quit: Else");
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+            switch(number){
+                case 0:
+                    solve = new MinConflicts();
+                    break;
+                case 1:
+                    solve = new BackTrackingS();
+                    break;
+                case 2:
+                    solve = new BackTrackingFC();
+                    break;
+                case 3:
+                    solve = new BackTrackingMAC();
+                    break;
+                case 4:
+                    solve = new GeneticAlgo();
+                    break;
+                default:
+                    run = false;
+                    break;
             }
-            System.out.println();
-            g = new Graph(i);
-            g = solve.solve(g, 4);
-            System.out.println("Number of Vertices: " + i + " Number of Colors: 4");
-            if(g.getVertices() == null){
-                System.out.print("Graph could not be colored in 4 colors.");
-            }else{
-                if(solve.evaluate(g)) System.out.print("The Graph was colored correctly.");
-                else System.out.print("The algorithm did not work properly.");
+            if(run){
+                for(int i = 10; i <= 100; i += 10){
+                    Graph g = new Graph(i);
+                    g = solve.solve(g, 3);
+                    System.out.print("Number of Vertices: " + i + " Number of Colors: 3 Coloring: ");
+                    if(g.getVertices() == null){
+                        System.out.print("Not Possible");
+                    }else{
+                        if(solve.evaluate(g)) System.out.print("Correct");
+                        else System.out.print("Error");
+                    }
+                    System.out.println("\n");
+                    g = new Graph(i);
+                    g = solve.solve(g, 4);
+                    System.out.print("Number of Vertices: " + i + " Number of Colors: 4 Coloring: ");
+                    if(g.getVertices() == null){
+                        System.out.print("Not Possible");
+                    }else{
+                        if(solve.evaluate(g)) System.out.print("Correct");
+                        else System.out.print("Error");
+                    }
+                    System.out.println("\n");
+                }
             }
-            System.out.println();
         }
-    }
+    }   
 }
