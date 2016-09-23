1c2b9cbbf127476307d828e0168cce7a002e0753
LizzieHerman <lizziemn96@gmail.com>
2016-09-23 00:29:31 -0600

    Update BackTrackingS.java

--- BackTrackingS.java
+++ BackTrackingS.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package graphcoloring;
 
 import java.util.*;
 
 /**
 *
- * @author Lizzi
+ * @author Lizzie Herman
  */
 public class BackTrackingS extends Solver{
-
-    BackTrackingS() {
-        
-    }
     public Graph solve(Graph g, int k){
         ArrayList<Vertex> coloredVerts = backTrack(g.getVertices(), k);
         g.setColoredVerts(coloredVerts);
